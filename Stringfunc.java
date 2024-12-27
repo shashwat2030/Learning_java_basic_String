2246157ad8f7427769a4a387f02f4cd45af2a9a9
shashwat2030 <75948019+shashwat2030@users.noreply.github.com>
2024-12-27 21:57:39 +0530

    modifying string function and allocating sequence of functions

--- Stringfunc.java
+++ Stringfunc.java
@@ -8,21 +8,26 @@ public static void main(String []args)
 String s ="swEra_Shashwat";
 String success ="_is_Successful_IAS";
 String combination =s+success;
-System.out.println("length_of_string:"+combination.length());
-System.out.println("character_at_specified_position:"+combination.charAt(8));
-System.out.println("finding_substring_at_initial_stage:"+combination.substring(3));
-System.out.println("finding_substring_from_initial_to_final_stage:"+combination.substring(8,24));
-System.out.println("concatenation_operation:"+s.concat(success));
-System.out.println("index_operation_of_String:"+combination.indexOf("swEra_Shashwat"));
-System.out.println("index_of_string:"+combination.indexOf('s',8));
+System.out.println("String_Length:"+combination.length());
+System.out.println("Lower_Case:"+combination.toLowerCase());
+System.out.println("Upper_Case:"+combination.toUpperCase());
+System.out.println("Trim_String:"+combination.trim());
+System.out.println("Substring_with_Initial:"+combination.substring(10));
+System.out.println("Substring_with_Initial&final_index:"+combination.substring(8,19));
+System.out.println("Replace:"+success.replace('A','F'));
 int compare =s.compareTo(success);
 System.out.println("differene_between_string:"+compare);
-System.out.println(combination.toLowerCase());
-System.out.println(combination.toUpperCase());
-System.out.println(s.trim());
-String s1 ="feeksforfeeks";
-String s2 =s1.replace('f','g');
-System.out.println(s2);
+System.out.println("Starts_With:"+success.startsWith("su"));
+System.out.println("Ends_With:"+success.endsWith("ia"));
+System.out.println("Character_Index:"+success.charAt(8));
+System.out.println("String_index:"+success.indexOf(s));
+System.out.println("String_index_from_particular_Index:"+success.indexOf(s,3));
+System.out.println("Last_Index:"+success.lastIndexOf("S"));
+System.out.println("Last_Index:Before_index:"+success.lastIndexOf("s",3));
+System.out.println(s.equals(success));
+System.out.println(s.equalsIgnoreCase(success));
+System.out.println(s.concat(success));
+
 
 
 
